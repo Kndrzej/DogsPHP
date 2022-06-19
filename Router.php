0046a6be61af9d917d6f377631574dfec35b1a44
Kndrzej <kozinskiandrzej4@gmail.com>
2022-06-19 17:33:37 +0200

    Docker setup

---
+++ Router.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class Router {
+    public static $routes;
+
+    public static function get($url, $controller){
+        self::$routes[$url] = $controller;
+    }
+
+    public static function run($url) {
+        $urlParts = explode('/', $url);
+        $action = $urlParts[0];
+
+        if(!array_key_exists($action, self::$routes)) {
+            header("Location: index");
+            die();
+        }
+
+        $controller = self::$routes[$action];
+        $object = new $controller;
+        $action = $action ?: 'index';
+        $id = $urlParts[1] ?? ''; // ?? czy wartośc istnieje czy nie, jesli nie wstaw znak pusty
+
+        $object->$action($id);
+
+    }
+}
