9d1e62a8f5b1240e7113b1ee866f4953b06e670b
Kndrzej <kozinskiandrzej4@gmail.com>
2022-06-30 08:28:44 +0200

    created sql dump

--- dump/d39gdcn5obgk9d_public_users.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO public.users (id, email, password, id_user_details, admin) VALUES (38, 'johnsnow@pk.edu.pl', '$2y$10$0ycmgFW.3q4e/3AJtS0yYOz2mLzSwjeYRmL09vudNt2axs9IG.4aK', 4, false);
-INSERT INTO public.users (id, email, password, id_user_details, admin) VALUES (39, 'johnsnow@pk.edu.pl', '$2y$10$FyNDrEbYInGK6hIOf4gVfeqMVPl6bCVvUyS7adoKU/V/A8EUEpVLO', 4, false);
-INSERT INTO public.users (id, email, password, id_user_details, admin) VALUES (40, 'andrzej@gmail.com', '$2y$10$f.C/YLCtbMX8QtCh9Qptiu18ss5ea0IezOty66RA/zwU8TAXqNCXO', 6, true);
-INSERT INTO public.users (id, email, password, id_user_details, admin) VALUES (41, 'andrzej@gmail.com', '$2y$10$qPxt04xOesb6RQll2ObbbeUwbVyzfvY3ALm78AXBXKBZ9X/ABGsi2', 6, true);
-INSERT INTO public.users (id, email, password, id_user_details, admin) VALUES (42, 'kozinskiandrzej4@gmail.com', '$2y$10$oMSiEKsk070xby584hQdEOTtTgigjPKQ5.aoa5oG78QADzcwLWwES', 12, true);
